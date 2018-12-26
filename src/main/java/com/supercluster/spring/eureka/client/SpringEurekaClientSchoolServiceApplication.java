2b5e3203437f6c946a53bc0b533d62b4d7118727
Karthik <springcoding@gmail.com>
2018-12-26 08:27:11 -0500

    Spring eureka client school service/Initial commit/12-26-2018

---
+++ src/main/java/com/supercluster/spring/eureka/client/SpringEurekaClientSchoolServiceApplication.java
@@ -0,0 +1,16 @@
+package com.supercluster.spring.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringEurekaClientSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaClientSchoolServiceApplication.class, args);
+	}
+
+}
+
